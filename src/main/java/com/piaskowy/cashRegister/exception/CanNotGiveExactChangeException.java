a175e9704c13459fad268a65f78687ce1cae8bb6
Jan Piaskowy <piaskowyjasiek@gmail.com>
2023-07-05 14:05:16 +0200

    fix: removed duplicate package

--- src/main/java/com/piaskowy/cashRegister/exception/CanNotGiveExactChangeException.java
+++
@@ -1,8 +0,0 @@
-package com.piaskowy.cashRegister.exception;
-
-public class CanNotGiveExactChangeException extends RuntimeException {
-
-    public CanNotGiveExactChangeException(final String message) {
-        super(message);
-    }
-}
--- src/main/java/com/piaskowy/cashRegister/exception/NotEnoughMoneyException.java
+++
@@ -1,7 +0,0 @@
-package com.piaskowy.cashRegister.exception;
-
-public class NotEnoughMoneyException extends RuntimeException {
-    public NotEnoughMoneyException(final String message) {
-        super(message);
-    }
-}
