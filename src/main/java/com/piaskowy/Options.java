d29ecb5f6870eaee8793fd1c96d90d90d804e4f6
Jan Piaskowy <piaskowyjasiek@gmail.com>
2023-07-05 13:45:30 +0200

    fix: fixed run option name - Renamed misspelled variable

--- src/main/java/com/piaskowy/Options.java
+++ src/main/java/com/piaskowy/Options.java
@@ -5,6 +5,6 @@ import com.piaskowy.commandLineArgument.Option;
 import java.util.List;
 
 public class Options {
-    @Option(name = "--moeny", type = List.class, description = "Opcja jest wymagana", required = true)
+    @Option(name = "--change", type = List.class, description = "Opcja jest wymagana", required = true)
     public List<Double> files;
 }
